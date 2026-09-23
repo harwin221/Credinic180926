6ba99654adeaeb5cd66942e75d101943ec031786
harwin221 <harwinrueda221@gmail.com>
2026-09-23 20:23:57 +0000

    fix(api): import Carbon namespace in MobileApiController

--- app/Http/Controllers/MobileApiController.php
+++ app/Http/Controllers/MobileApiController.php
@@ -7,6 +7,7 @@ use App\Models\prestamoCuotasModel;
 use App\Models\prestamoCuotaAbonoModel;
 use App\Models\prestamosModel;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
